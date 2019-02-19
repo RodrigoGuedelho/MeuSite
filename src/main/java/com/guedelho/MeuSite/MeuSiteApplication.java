ad9e8db4b819bbaaf053e81a50ed93ffd8a7cfb4
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2019-02-18 23:00:26 -0300

    - Commit inícial do projeto ;

---
+++ src/main/java/com/guedelho/MeuSite/MeuSiteApplication.java
@@ -0,0 +1,13 @@
+package com.guedelho.MeuSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuSiteApplication.class, args);
+	}
+
+}
